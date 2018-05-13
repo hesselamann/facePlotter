aa6d5a47753fe8c3be3335696e022dc4be890e37
Hessel Bearda <hesselrb@hotmail.com>
2018-05-13 20:10:22 +0200

    feat(main): add rotation of image

--- js/main.js
+++ js/main.js
@@ -1,15 +1,47 @@
 // Create a raster item using the image tag with id='face'
-var raster = new Raster('face');
+var image = document.getElementById('face');
+var canvas = document.getElementById('myCanvas');
+var canvasContext = canvas.getContext('2d');
+
+var TO_RADIANS = Math.PI/180; 
+function drawRotatedImage(image, x, y, angle)
+{ 
+    // save the current co-ordinate system 
+    // before we screw with it
+    canvasContext.save(); 
+
+    // move to the middle of where we want to draw our image
+    canvasContext.translate(x, y);
+
+    // rotate around that point, converting our 
+    // angle from degrees to radians 
+    canvasContext.rotate(angle * TO_RADIANS);
+
+    // draw it up and to the left by half the width
+    // and height of the image 
+    canvasContext.drawImage(image, -(image.width/2), -(image.height/2));
+
+    // and restore the co-ords to how they were when we began
+    canvasContext.restore(); 
+}
+ 
+drawRotatedImage(image, 600, 450, 15);
+
+var imgURL = canvas.toDataURL();
+
+var raster = new Raster({
+	source: imgURL
+});
 
 // Hide the raster
-raster.visible = false;
+raster.visible = true;
 
 // The size of our grid cells
-var gridSize = 10;
+var gridSize = 15;
 
 // Raster dimensions
-var rasterWidth = 120;
-var rasterHeight = 90;
+var rasterWidth = 60;
+var rasterHeight = 45;
 
 // Space the cells by 100%
 var spacing = 1;
@@ -22,9 +54,11 @@ var segmentsPerGridCell = phasesPerGridCell * segmentsPerPhase;
 // As the web is asynchronous, we need to wait for the raster to load
 // before we can perform any operation on its pixels.
 raster.on('load', function() {
+
 	// Since the example image we're using is much too large,
 	// and therefore has way too many pixels, lets downsize it to
 	// 60 pixels wide and 45 pixels high:
+
 	raster.size = new Size(rasterWidth, rasterHeight);
 
 	for (var y = 0; y < raster.height; y++) {
@@ -32,6 +66,7 @@ raster.on('load', function() {
 		for(var x = 0; x < raster.width; x++) {
 			
 			// Get the color of the pixel:
+			
 			var color = raster.getPixel(x, y);
 
 			// Create a circle shaped path:
@@ -69,7 +104,8 @@ raster.on('load', function() {
 			}
 
 			// Smooth the paths points
-			path.smooth()
+			//path.rotate(30, view.center);
+			path.smooth();
 
 		}
 	}
@@ -77,4 +113,5 @@ raster.on('load', function() {
 	// Move the active layer to the center of the view, so all 
 	// the created paths in it appear centered.
 	project.activeLayer.position = view.center;
+	project.activeLayer.rotate(-15);
 });
